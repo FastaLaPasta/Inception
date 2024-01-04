342a4e284e452953268e415f26f7f1d95f6e4e78
boulogne <boulogne@Inception.myguest.virtualbox.org>
2024-01-04 19:51:40 +0100

    wordpress at the end

---
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#IF LE FICHIER N'existe PAS A RAJOUTER
+
+sleep 10;
+cd /var/www/wordpress
+wp config create	--allow-root \
+    --dbname=$SQL_DATABASE \
+    --dbuser=$SQL_USER \
+    --dbpass=$SQL_PASSWORD \
+    --dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp core install     --allow-root \
+    --url='sboulogn.42.fr' \
+    --title='Inception' \
+    --admin_user=$SQL_USER \
+    --admin_password=$SQL_PASSWORD \
+    --admin_email='sashaboulogne@gmail.com'
+wp user create --allow-root leBgQuiMeCorrige lebg@gmail.com --porcelain\
+
+mkdir -p /run/php
+/usr/sbin/php-fpm7.3 -F
+
+
